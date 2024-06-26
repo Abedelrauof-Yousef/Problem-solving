1862bfd39995e3d1d4c8dfb45f2f6bca313abf43
Abedelrauof <abedelrauofyousef@gmail.com>
2024-06-27 00:12:27 +0300

    ex3 only

---
+++ app.js
@@ -0,0 +1,12 @@
+// Ex3
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9 , 10];
+// let total = 0;
+// function recrusion(arr, index = 0){
+//     if(index >= arr.length){
+//         return total;
+//     }
+//     total += arr[index];
+//     return recrusion(arr, index + 1);
+// }
+// let sum = recrusion(arr, index = 0);
+// console.log(sum);
